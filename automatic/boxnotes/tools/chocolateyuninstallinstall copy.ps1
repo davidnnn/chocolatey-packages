5fafe951dddbecda26a9064278ec72be9015281f
Unknown <david_live-2-ride@hotmail.com>
2018-05-11 16:50:23 -0700

    fix
    iconurl
    uninstall

---
+++ automatic/boxnotes/tools/chocolateyuninstallinstall copy.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = "boxnotes"
+$installerType = "exe"
+
+
+# Uninstall PDFCreator if older version is installed
+if (Test-Path "$env:LOCALAPPDATA\boxnotes") {
+    Uninstall-ChocolateyPackage $packageName $installerType "--uninstall" "$env:LOCALAPPDATA\boxnotes\update.exe"
+}
