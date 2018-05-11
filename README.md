f8086c8d8a5652091654d6947db9d18276b9dbde
Unknown <david_live-2-ride@hotmail.com>
2018-05-11 16:02:46 -0700

    init

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ~~~
 <!-- EDIT ME-->
 
-[![](https://ci.appveyor.com/api/projects/status/github/YOUR_GITHUB_USERNAME_HERE/chocolatey-packages?svg=true)](https://ci.appveyor.com/project/YOUR_GITHUB_USERNAME_HERE/chocolatey-packages)
-[Update status](https://gist.github.com/YOUR_GITHUB_USERNAME_HERE/YOUR_GIST_ID)
+[![](https://ci.appveyor.com/api/projects/status/github/davidnnn/chocolatey-packages?svg=true)](https://ci.appveyor.com/project/davidnnn/chocolatey-packages)
+[Update status](https://gist.github.com/davidnnn/YOUR_GIST_ID)
 
 <!-- REMOVE THE squiggles "~" surrounding this (this should not be a code block) -->
 ~~~
---
+++ automatic/boxnotes/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'boxnotes' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://e3.boxcdn.net/box-installers/boxnotes/win/latest/BoxNotesSetup.exe'
+  softwareName  = 'boxnotes*'
+  checksum      = '2435b4ad40d447161b5ee1b4e76103345f0d8d4737abbdc0ffca74533bb5d771'
+  checksumType  = 'sha256'
+  silentArgs   = '/install /quiet /norestart'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
